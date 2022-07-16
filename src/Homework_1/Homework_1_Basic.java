61eeca48743e9bc568119ec84e9acbccb164949f
vital <vital>
2022-07-16 13:40:18 +0300

    Начало

---
+++ src/Homework_1/Homework_1_Basic.java
@@ -0,0 +1,12 @@
+package Homework_1;
+
+public class Homework_1_Basic {
+    public static void main(String[] args) {
+        String hi = "                Hello ";
+        String world = " WoRld!";
+        char newLine = '\n';
+        String.trim(hi);
+        char[] greeting = hi + world;
+        System.out.println(greeting);
+    }
+}
