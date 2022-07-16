adbd70446bfa7723275e1fc868d15acb287c30a2
vital <vital>
2022-07-16 15:47:19 +0300

    Первое задание

--- src/Homework_1/Homework_1_Basic.java
+++ src/Homework_1/Homework_1_Basic.java
@@ -1,12 +1,16 @@
+//Базовый уровень
 package Homework_1;
+
+import java.util.Locale;
 
 public class Homework_1_Basic {
     public static void main(String[] args) {
+        //Задача №1
         String hi = "                Hello ";
         String world = " WoRld!";
         char newLine = '\n';
-        String.trim(hi);
-        char[] greeting = hi + world;
-        System.out.println(greeting);
+        String hey = hi.trim();
+        String worldSmall = world.toLowerCase();
+        System.out.println(hey + worldSmall);
     }
 }
