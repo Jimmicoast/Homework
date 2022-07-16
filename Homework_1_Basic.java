a76271b2417a4305155db4d2db081f8ae5a6deab
Jimmicoast <109170421+Jimmicoast@users.noreply.github.com>
2022-07-16 17:32:20 +0300

    Add files via upload

---
+++ Homework_1_Basic.java
@@ -0,0 +1,32 @@
+package Homework_1;
+public class Homework_1_Basic {
+    public static void main(String[] args) {
+        //Базовый уровень
+        //Задача №1
+        String hi = "                Hello ";
+        String world = " WoRld!";
+        char newLine = '\n';
+        String hey = hi.trim();
+        String worldSmall = world.toLowerCase();
+        String result = hey + worldSmall + newLine;
+        String resultMulti = result.repeat(3);
+        System.out.println(resultMulti);
+
+        //Задача №2
+        float height = 1.81f;
+        int weight = 63;
+        float indexHuman = height / (weight * weight);
+        System.out.printf("Мой индекс массы тела - %.6f\n", indexHuman);
+
+        //Задача №3
+        char[] alphabet = {'a', 'b', 'c', 'd', 'e'};
+        System.out.println(alphabet);
+        alphabet[3] = 'h';
+        System.out.println(alphabet);
+
+        //Продвинутый уровень
+        //Задача №1
+        
+
+    }
+}
