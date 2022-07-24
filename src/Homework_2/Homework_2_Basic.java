8ef9113807c854a90569dc9c4f498710107d08f7
vital <vital>
2022-07-24 13:31:14 +0300

    Домашнее задание №2 версия 1

---
+++ src/Homework_2/Homework_2_Basic.java
@@ -0,0 +1,14 @@
+package Homework_2;
+
+public class Homework_2_Basic {
+    public static void main(String[] args) {
+        //Базовый уровень
+        //Задача №1
+        for (int i = 1; i < 101; i = i +1) {
+            System.out.print(i + "a" + " ");
+        }
+
+
+    }
+
+}
