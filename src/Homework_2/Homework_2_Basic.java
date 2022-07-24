da4816a2488170ca9686284c7b5cacc1e242de95
vital <vital>
2022-07-24 13:51:15 +0300

    Домашнее задание №2 версия 2

--- src/Homework_2/Homework_2_Basic.java
+++ src/Homework_2/Homework_2_Basic.java
@@ -7,7 +7,19 @@ public class Homework_2_Basic {
         for (int i = 1; i < 101; i = i +1) {
             System.out.print(i + "a" + " ");
         }
-
+        //Задача №2
+        int ageChildren = 10;
+        if (ageChildren < 6) {
+            System.out.println("\nРебенок пошел в детский сад");
+        }
+        else if (ageChildren >= 6 && ageChildren < 11) {
+            System.out.println("\nРебенок пошел в младшую школу");
+        } else if (ageChildren >= 11 && ageChildren < 17) {
+            System.out.println("\nРебенок пошел в среднюю школу");
+        }
+        else {
+            System.out.println("\nРебенок пошел в университет");
+        }
 
     }
 
